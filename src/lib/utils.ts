0842480b6599f0071685aeb1576ed21c968f2d7c
Rajneesxx <rajneeshpande.0@gmail.com>
2025-08-21 16:43:10 +0530

    Update utils.ts

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,3 +4,27 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+// Copy extracted JSON to clipboard
+export const copyToClipboard = async (data: object) => {
+  try {
+    await navigator.clipboard.writeText(JSON.stringify(data, null, 2));
+    alert("Copied to clipboard!");
+  } catch (err) {
+    console.error("Failed to copy", err);
+  }
+};
+
+// Download extracted JSON as file
+export const downloadJSON = (data: object, filename = "extracted-data.json") => {
+  const blob = new Blob([JSON.stringify(data, null, 2)], {
+    type: "application/json",
+  });
+  const url = URL.createObjectURL(blob);
+
+  const a = document.createElement("a");
+  a.href = url;
+  a.download = filename;
+  a.click();
+
+  URL.revokeObjectURL(url);
+};
